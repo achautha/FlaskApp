2b560ff2d56d376b9167df72e9f17987c665a608
Atharva Chauthaiwale <atharvac@juniper.net>
2016-12-27 16:04:26 +0530

    change hello message

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello, I love Digital Ocean!"
+    return "Hello, I love AWS"
 if __name__ == "__main__":
     app.run()
